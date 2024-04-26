0412d89c12f1482a2bb17ff1a97b2487884e0450
gulnar <salehovagulnar64@gmail.com>
2024-04-26 22:20:56 +0400

    *

--- src/main/java/aze/coders/spring/mvc/controller/CustomersController.java
+++ src/main/java/aze/coders/spring/mvc/controller/CustomersController.java
@@ -1,2 +1,14 @@
-package aze.coders.spring.mvc.controller;public class CustomersController {
+package aze.coders.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomersController {
+
+    @GetMapping
+    public String customers() {
+        System.out.println("customer page");
+        return "customers";
+    }
 }
--- src/main/java/aze/coders/spring/mvc/controller/Main.java
+++ src/main/java/aze/coders/spring/mvc/controller/Main.java
@@ -1,2 +1,7 @@
-package aze.coders.spring.mvc.controller;public class Main {
+package aze.coders.spring.mvc.controller;
+
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+public class Main {
+//    InternalResourceViewResolver
 }
